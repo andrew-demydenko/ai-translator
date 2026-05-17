503259bc5a659a5646846df47c1da646da587028
Andrew Demydenko <and.demydenko@gmail.com>
2026-05-17 23:18:10 +0300

    fix WS_URL

--- apps/web/src/shared/config/constats.ts
+++ apps/web/src/shared/config/constats.ts
@@ -1,8 +1,9 @@
 export const API_URL: string =
   import.meta.env.VITE_API_URL ||
-  `${window.location.protocol}://${window.location.host}/api`;
+  `${window.location.protocol}//${window.location.host}/api`;
 
 const wspProtocol = window.location.protocol === "https:" ? "wss:" : "ws:";
-export const WS_URL: string =
-  API_URL.replace(window.location.protocol, wspProtocol) ||
-  `${wspProtocol}//${window.location.host}`;
+export const WS_URL: string = API_URL.replace(
+  window.location.protocol,
+  wspProtocol,
+);
