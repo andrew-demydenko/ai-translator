5e572adb22ede1364f1266867d20533913594fae
Andrew Demydenko <and.demydenko@gmail.com>
2026-05-23 10:40:18 +0300

    refactor(topic): remove redundant useTopics hook

--- apps/web/src/entities/topic/index.ts
+++ apps/web/src/entities/topic/index.ts
@@ -1,4 +1,3 @@
 export * from "./model/types";
-export * from "./model/useTopics";
 export { useTopicStore } from "./model/topic.store";
 export * from "./ui/TopicChip";
--- apps/web/src/entities/topic/model/useTopics.ts
+++
@@ -1,9 +0,0 @@
-import { useTopicStore } from "./topic.store";
-
-export const useTopics = () => {
-  const topics = useTopicStore((s) => s.topics);
-  const addTopic = useTopicStore((s) => s.addTopic);
-  const deleteTopic = useTopicStore((s) => s.deleteTopic);
-
-  return { topics, addTopic, deleteTopic };
-};
